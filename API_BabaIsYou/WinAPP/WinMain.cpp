3fba7f747cd2f93e715f603479e145c42334e29a
rudtlrdl96 <rudtlrdl96@gmail.com>
2023-01-02 18:54:00 +0900

    김경식 : 프로젝트 이름 수정

--- API_BabaIsYou/WinAPP/WinMain.cpp
+++ API_BabaIsYou/WinAPP/WinMain.cpp
@@ -7,15 +7,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-
 	GameEngineDebug::LeakCheck();
-
 	GameEngineWindow::WindowCreate(hInstance, "MainWindow");
-
 	GameEngineWindow::WindowLoop();
-
-	// main이 끝나면 무조건 끝나기 때문에
-	// 무조건 붙잡아둬야 합니다.
 
 	return 1;
 }
