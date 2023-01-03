6402dbc25d3fda7d2db0cc49c1bb41ed78dc3c43
rudtlrdl96 <rudtlrdl96@gmail.com>
2023-01-03 19:48:13 +0900

    김경식 : 엔진업데이트 및 리소스 수정

--- API_BabaIsYou/GameEnginePlatform/GameEngineWindow.cpp
+++ API_BabaIsYou/GameEnginePlatform/GameEngineWindow.cpp
@@ -3,13 +3,25 @@
 
 // LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM)
 
-HWND GameEngineWindow::hWnd = nullptr;
+HWND GameEngineWindow::HWnd = nullptr;
+HDC GameEngineWindow::DrawHdc = nullptr;
+float4 GameEngineWindow::WindowSize = { 800, 600 };
+float4 GameEngineWindow::WindowPos = { 100, 100 };
+float4 GameEngineWindow::ScreenSize = { 800, 600 };
+
+
+bool IsWindowUpdate = true;
 
 LRESULT CALLBACK MessageFunction(HWND _hWnd, UINT _message, WPARAM _wParam, LPARAM _lParam)
 {
     switch (_message)
     {
-        // 내 윈도우가 선택되었다.
+    case WM_MOUSEMOVE:
+    {
+        int a = 0;
+        break;
+    }
+    // 내 윈도우가 선택되었다.
     case WM_SETFOCUS:
     {
         int a = 0;
@@ -29,6 +41,7 @@ LRESULT CALLBACK MessageFunction(HWND _hWnd, UINT _message, WPARAM _wParam, LPAR
     {
         // Message함수가 0을 리턴하게 만들어라.
         PostQuitMessage(0);
+        IsWindowUpdate = false;
         break;
     }
     default:
@@ -47,8 +60,10 @@ GameEngineWindow::~GameEngineWindow()
 }
 
 
-void GameEngineWindow::WindowCreate(HINSTANCE _hInstance, const std::string_view& _TitleName)
+void GameEngineWindow::WindowCreate(HINSTANCE _hInstance, const std::string_view& _TitleName, float4 _Size, float4 _Pos)
 {
+    // 윈도우를 찍어낼수 있는 class를 만들어내는 것이다.
+    // 나는 이러이러한 윈도우를 만들어줘...
     WNDCLASSEX wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
@@ -58,7 +73,8 @@ void GameEngineWindow::WindowCreate(HINSTANCE _hInstance, const std::string_view
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
     wcex.hInstance = _hInstance;
-    wcex.hIcon = nullptr;  //LoadIcon(_hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
+    // 넣어주지 않으면 윈도우 기본Icon이 됩니다.
+    wcex.hIcon = nullptr;//LoadIcon(_hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
     wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // 흰색 
     wcex.lpszMenuName = nullptr;//MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
@@ -79,45 +95,96 @@ void GameEngineWindow::WindowCreate(HINSTANCE _hInstance, const std::string_view
     // 윈도우는 다시 특정 숫자이라는 윈도우가 만들어졌다고 우리에게 알려주는데.
     // 특정 숫자로 인식되는 우리의 윈도우에게 크기변경 떠라
 
-    hWnd = CreateWindow("GameEngineWindowDefault", _TitleName.data(), WS_OVERLAPPEDWINDOW,
+    // (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX)
+
+    HWnd = CreateWindow("GameEngineWindowDefault", _TitleName.data(), WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, _hInstance, nullptr);
 
-    if (!hWnd)
+    if (!HWnd)
     {
         MsgAssert("윈도우 클래스 생성에 실패했습니다.");
         return;
     }
 
-    ShowWindow(hWnd, SW_SHOW);
-    UpdateWindow(hWnd);
+    DrawHdc = GetDC(HWnd);
+
+    ShowWindow(HWnd, SW_SHOW);
+    UpdateWindow(HWnd);
+
+    SettingWindowSize(_Size);
+    SettingWindowPos(_Pos);
 
     return;
 }
 
-int GameEngineWindow::WindowLoop()
+int GameEngineWindow::WindowLoop(void(*_Start)(), void(*_Loop)(), void(*_End)())
 {
+    // 단축키인데. 안써도 문제가 되지는 않는다.
     // HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
 
+    if (nullptr != _Start)
+    {
+        _Start();
+    }
+
+
     MSG msg;
-    
-    while (GetMessage(&msg, nullptr, 0, 0))
+
+    // 동기 함수가 종료될때까지 프로그램이 멈춘다.
+
+    // 기본 메시지 루프입니다:
+    // GetMessage는 내 윈도우에 무슨일이 생기는지 체크해줘.
+    // GetMessage는 윈도우의 특별한 일이 생길대까지 멈추는 함수인겁니다.
+    while (IsWindowUpdate)
     {
         //if (!TranslateAccelerator(msg.hwnd, nullptr, &msg))
         //{
         //}
 
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        // 윈도우 메세지를 처리한다.
+        // GetMessage는 동기함수이기 때문에 애초에 게임을 만들수 있는 메세지 방식이 아니다
+        // => 게임은 쉴새없이 돌아야 하는데
+        // GetMessage라는 함수는 => 윈도우에 무슨일이 생기면 리턴되는 함수
+        // 윈도우에 무슨일이 생기게 만들어야 한다.
+        if (GetMessage(&msg, nullptr, 0, 0))
+        {
+            if (nullptr != _Loop)
+            {
+                _Loop();
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    if (nullptr != _End)
+    {
+        _End();
     }
 
     return (int)msg.wParam;
 }
 
-void GameEngineWindow::WindowSize(float4 _Size)
+void GameEngineWindow::SettingWindowSize(float4 _Size)
 {
 
+    // 그 타이틀바와 프레임까지 고려해서 크기를 설정해줘야 한다.
+
+    //          위치      크기
+    RECT Rc = { 0, 0, _Size.ix(), _Size.iy() };
+
+    ScreenSize = _Size;
+
+    // 내가 원하는 크기를 넣으면 타이틀바까지 고려한 크기를 리턴주는 함수.
+    AdjustWindowRect(&Rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    WindowSize = { static_cast<float>(Rc.right - Rc.left), static_cast<float>(Rc.bottom - Rc.top) };
+    // 0을 넣어주면 기존의 크기를 유지한다.
+    SetWindowPos(HWnd, nullptr, WindowPos.ix(), WindowPos.iy(), WindowSize.ix(), WindowSize.iy(), SWP_NOZORDER);
 }
-void GameEngineWindow::WindowPos(float4 _Pos)
+void GameEngineWindow::SettingWindowPos(float4 _Pos)
 {
-
+    WindowPos = _Pos;
+    SetWindowPos(HWnd, nullptr, WindowPos.ix(), WindowPos.iy(), WindowSize.ix(), WindowSize.iy(), SWP_NOZORDER);
 }
--- API_BabaIsYou/WinAPP/WinMain.cpp
+++ API_BabaIsYou/WinAPP/WinMain.cpp
@@ -1,6 +1,32 @@
 #include <Windows.h>
 #include <GameEngineBase/GameEngineDebug.h>
 #include <GameEnginePlatform/GameEngineWindow.h>
+#include <GameEngineBase/GameEngineMath.h>
+
+void TestGameStart()
+{
+	int a = 0;
+}
+
+int x = 0;
+
+void TestGameLoop()
+{
+	// 화면에 그림을 그려주는 함수입니다.
+
+	++x;
+
+	Rectangle(GameEngineWindow::GetDrawHdc(), 0 + x, 0, 100 + x, 100);
+
+	// 몬스터가 움직이게 만들고
+	// 플레이어가 움직이게 만들어야 한다.
+	int a = 0;
+}
+
+void TestGameEnd()
+{
+	int a = 0;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -8,8 +34,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ int       nCmdShow)
 {
 	GameEngineDebug::LeakCheck();
-	GameEngineWindow::WindowCreate(hInstance, "MainWindow");
-	GameEngineWindow::WindowLoop();
+
+	GameEngineWindow::WindowCreate(hInstance, "MainWindow", { 1280, 720 }, { 0, 0 });
+
+	GameEngineWindow::WindowLoop(TestGameStart, TestGameLoop, TestGameEnd);
+
+	// main이 끝나면 무조건 끝나기 때문에
+	// 무조건 붙잡아둬야 합니다.
 
 	return 1;
 }
