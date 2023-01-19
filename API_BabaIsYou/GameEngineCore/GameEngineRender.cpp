00a9c2f627aba01e8b3c2687bf850ce97fa47291
rudtlrdl96 <rudtlrdl96@gmail.com>
2023-01-19 19:30:36 +0900

    김경식 : 엔진업데이트

--- API_BabaIsYou/GameEngineCore/GameEngineRender.cpp
+++ API_BabaIsYou/GameEngineCore/GameEngineRender.cpp
@@ -64,8 +64,7 @@ void GameEngineRender::FrameAnimation::Render(float _DeltaTime)
 			{
 				CurrentIndex = 0;
 			}
-			else 
-			{
+			else {
 				CurrentIndex = static_cast<int>(FrameIndex.size()) - 1;
 			}
 		}
@@ -76,11 +75,17 @@ void GameEngineRender::FrameAnimation::Render(float _DeltaTime)
 
 void GameEngineRender::Render(float _DeltaTime)
 {
+
 	if (nullptr != CurrentAnimation)
 	{
 		CurrentAnimation->Render(_DeltaTime);
 		Frame = CurrentAnimation->FrameIndex[CurrentAnimation->CurrentIndex];
 		Image = CurrentAnimation->Image;
+	}
+
+	if (nullptr == Image)
+	{
+		MsgAssert("이미지를 세팅해주지 않았습니다.");
 	}
 
 	float4 CameraPos = float4::Zero;
